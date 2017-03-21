3af4853d8f47f529a7869d94771a8e4b639e61e6
AWen <AWen@AWen-PC>
2017-03-22 00:39:46 +0800

    2

--- src/main/java/com/ium/um/Application.java
+++ src/main/java/com/ium/um/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-@SpringBootApplication
+//@SpringBootApplication
 //implements CommandLineRunner
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+		System.out.println( 0.05 + 0.02);
+		System.out.println( (float)(0.05 + 0.001));
 	}
 
 }
