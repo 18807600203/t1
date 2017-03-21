3da70ec7a654dc5dddcd64968d72a80221d5f7c4
AWendy <18807600203@139.com>
2017-03-21 17:53:19 +0800

    1

--- src/main/java/com/ium/um/mapper/BatteryInfoMapper.java
+++ src/main/java/com/ium/um/mapper/BatteryInfoMapper.java
@@ -14,7 +14,7 @@ public interface  BatteryInfoMapper {
 	@Select("select * from base where id = #{id}")
 	BatteryInfo findById( Long id);
 	
-	@Select("select * from base")
+	@Select("select * from base order by id asc")
 	List<BatteryInfo> getAll();
 	
 	@Insert("insert into base(chassis, channel, module) values( #{0}, #(1), #{2})")
