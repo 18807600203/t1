7fda4e6b6b214192c4eaa7cf02f610ae75ae691e
18807600203 <18807600203@139.com>
2017-03-29 17:33:46 +0800

    d

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,14 @@
-var app = angular.module('index',[]);
+var app = angular.module('index',['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
 app.run(function($templateCache) { //表格看模板缓存
   $templateCache.put('template.html', 
     '<tr>'+
         '<td ng-repeat="n in [] | range:20">' +
-            '123'+
+            '<div class="progress progress-bar-vertical" uib-popover="the row is : 123" popover-trigger="&#39;mouseenter&#39;">'+
+                '<div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="height: 30%;">30%'+
+                    '<span class="sr-only">30% Complete</span>'+
+                '</div>'+
+            '</div>'+
         '</td>'+
     '</tr>'
     );
@@ -35,6 +39,7 @@ app.filter('range', function() { //循环输出
 
 app.controller('indexCtrl', function($scope){
     $scope.test = '1234';
+    
 });
 
 
