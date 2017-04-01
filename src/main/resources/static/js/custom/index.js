005b0c3df94378e31ecea703c18f856562f4617e
18807600203 <18807600203@139.com>
2017-04-01 18:02:03 +0800

    改回了使用vue 前端框架
    寻找UI框架

--- src/main/resources/static/js/custom/index.js
+++ src/main/resources/static/js/custom/index.js
@@ -1,75 +1,68 @@
-var app = angular.module('index',['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+// var app = angular.module('index',['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-app.run(function($templateCache){
-    $templateCache.put('tabletemplate.html',
-        '<div ng-class="$index%2==0?&#39;doorA&#39;:&#39;doorB&#39;"><strong>{{doors[$index]}}</strong></div>'+  
-            '<table>'+
-               '<tr my-td ng-repeat="n in [] | range:8"></tr>'+
-        '</table>'
-    );
+// app.run(function($templateCache){
+//     $templateCache.put('tabletemplate.html',
+//         '<div ng-class="$index%2==0?&#39;doorA&#39;:&#39;doorB&#39;"><strong>{{doors[$index]}}</strong></div>'+  
+//             '<table>'+
+//                '<tr my-td ng-repeat="n in [] | range:8"></tr>'+
+//         '</table>'
+//     );
+// })
+
+// app.run(function($templateCache) { //表格看模板缓存
+//     $templateCache.put('template.html', 
+//         '<tr>'+
+//             '<td ng-repeat="n in [] | range:20">' +
+//                 '<div class="progress progress-bar-vertical"'+
+//                     'uib-tooltip-html= "&#39;<strong>{{doors[$parent.$parent.$index]}}</strong><br><strong>行号:</strong>{{$parent.$index + 1}}<br><strong>列号:</strong>{{$index + 1}}<br>&#39;">'+
+//                     '<div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="height: 30%;">30%'+
+//                         '<span class="sr-only">30% Complete</span>'+
+//                     '</div>'+
+//                 '</div>'+
+//             '</td>'+
+//         '</tr>'
+//     );
+// });
+//     $http({
+//         method: 'GET',
+//         url: '/index/left'
+//     }).then(function successCallback(response) {
+//             console.log(response.data);
+//         }, function errorCallback(response) {
+//             // 请求失败执行代码
+//     });
+
+// });
+
+Vue.component('my-table', {
+    template: '\
+        <div>\
+            <div :class = "[tableindex % 2 == 0 ? \'doorA\' : \'doorB\']"><strong>{{door}}</strong></div>\
+            <table>\
+                <my-tr v-for="(n,index) in 8" :key="index" :rowindex="index"></my-tr>\
+            </table>\
+        </div>\
+        ',
+    props:['door','tableindex'],
+})
+Vue.component('my-tr', {
+    template: '\
+    <tr>\
+        <td v-for="(n,index) in 20" :key="index">\
+            <div class="progress progress-bar-vertical" data-toggle="tooltip" data-placement="top" title="Tooltip on top">\
+                <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="height: 30%;">30%\
+                    <span class="sr-only">30% Complete</span>\
+                </div>\
+            </div>\
+        </td>\
+    </tr>\
+    ',
+    props:['rowindex'],
 })
 
-app.run(function($templateCache) { //表格看模板缓存
-    $templateCache.put('template.html', 
-        '<tr>'+
-            '<td ng-repeat="n in [] | range:20">' +
-                '<div class="progress progress-bar-vertical"'+
-                    'uib-tooltip-html= "&#39;<strong>{{doors[$parent.$parent.$index]}}</strong><br><strong>行号:</strong>{{$parent.$index + 1}}<br><strong>列号:</strong>{{$index + 1}}<br>&#39;">'+
-                    '<div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="height: 30%;">30%'+
-                        '<span class="sr-only">30% Complete</span>'+
-                    '</div>'+
-                '</div>'+
-            '</td>'+
-        '</tr>'
-    );
-});
-
-app.directive('myHello',function(){
-    return {
-        restrict : 'E',
-        replace : true,
-        template : '<div>hello angular</div>'
-    };
-});
-
-app.directive('myTd', function(){ //表格行模板
-    return{  
-        templateUrl : 'template.html'
-    };
-});
-
-app.directive('myTable', function(){
-    return{
-        templateUrl : 'tabletemplate.html'
+new Vue({
+    el: '#index',
+    data:{
+         doors:['A门', 'B门', 'C门', 'D门'],
     }
-})
-
-app.filter('range', function() { //循环输出
-  return function(input, total) {
-    total = parseInt(total);
-    for (var i=0; i<total; i++)
-      input.push(i);
-    return input;
-  };
 });
-
-app.controller('indexCtrl', function($scope,$http){
-    $scope.test = '1234';
-    $scope.doors = ['A门', 'B门', 'C门', 'D门'];
-    $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'];
-
-    $http({
-        method: 'GET',
-        url: '/index/left'
-    }).then(function successCallback(response) {
-            console.log(response.data);
-        }, function errorCallback(response) {
-            // 请求失败执行代码
-    });
-
-});
-
-
